d9bfe39537a3fc2c3df2ee9925ab92eaea959bcb
Walldoff Studios <Lethalbadger@hotmail.com>
2024-10-24 16:10:10 +0200

    Setup camera facade and installer, screen bounds classes are registered and setup within installer

---
+++ Assets/Asteroids/Scripts/CameraLogic/CameraFacade.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Asteroids
+{
+    public interface ICameraBoundsProvider
+    {
+        float OrthographicSize { get; }
+        float AspectRatio { get; }
+    }
+
+    
+    public class CameraFacade : ICameraBoundsProvider
+    {
+        private readonly Camera _camera;
+
+        public CameraFacade(Camera camera)
+        {
+            _camera = camera;
+        }
+
+        public float OrthographicSize => _camera.orthographicSize;
+        public float AspectRatio => _camera.aspect;
+    }   
+}
+
---
+++ Assets/Asteroids/Scripts/CameraLogic/CameraInstaller.cs
@@ -0,0 +1,31 @@
+using Asteroids.Borders;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Asteroids.CameraLogic
+{
+    public class CameraInstaller : LifetimeScope
+    {
+        [SerializeField] private Camera mainCam = null;
+        
+        protected override void Configure(IContainerBuilder builder)
+        {
+            base.Configure(builder);
+            builder.RegisterInstance(mainCam);
+
+            builder.Register<CameraFacade>(Lifetime.Singleton).As<ICameraBoundsProvider>();
+            
+            builder.Register<ScreenBoundsProvider>(Lifetime.Singleton)
+                .As<IScreenBoundsProvider>();
+            
+            builder.Register<ScreenWrapHandler>(Lifetime.Singleton)
+                .WithParameter(resolver => resolver.Resolve<IScreenBoundsProvider>());
+            
+            builder.Register<TransformWrapHandler>(Lifetime.Singleton)
+                .As<IRegisterWrappingTransform>()
+                .As<IUnregisterWrappingTransform>()
+                .As<IFixedTickable>();
+        }
+    }   
+}
